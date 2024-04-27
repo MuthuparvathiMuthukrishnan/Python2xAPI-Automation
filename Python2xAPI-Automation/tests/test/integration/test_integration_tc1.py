443c16dfb2b869554e21a5e9828ecfca41f542f7
My Name <myEmail@example.com>
2024-04-27 12:06:34 +0530

    API Automation - schema and crud operation

---
+++ Python2xAPI-Automation/tests/test/integration/test_integration_tc1.py
@@ -0,0 +1,13 @@
+#Integration Scenarios
+
+#1. verify that after create booking -> patch request - VErify the firstname is updated
+#2. Create a booking, delete it and Verify using GET request that it should not exist
+#3. Get existing booking id from GET All booking ids, Update a booking id and then Verify using GET by Id
+#4. Create a booking id and delete it
+#5. Invalid creation - enter a wron payload or Wrong JSON
+#6. Trying to update on a delete id -> 404
+
+#Test for the Single Req
+#Response
+#Headers
+#Status Code
